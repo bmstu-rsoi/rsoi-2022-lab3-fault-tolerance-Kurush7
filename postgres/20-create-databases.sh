349694f19ab1c318c3d55d0dc1f9b286f71ee6e2
Kurush7 <ze17@yandex.ru>
2022-12-11 23:06:48 +0300

    cicd wip

--- postgres/20-create-databases.sh
+++ postgres/20-create-databases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export VARIANT=4
+export VARIANT=v4
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
 psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
