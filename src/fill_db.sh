220698af888c7ab7686a9f4eac2ac4c0d1538a81
Kurush7 <ze17@yandex.ru>
2022-12-13 17:18:00 +0300

    cicd wip

--- src/fill_db.sh
+++ src/fill_db.sh
@@ -2,3 +2,7 @@ docker ps -a
 docker exec -i postgres psql -U program libraries < test/libraries.dump
 docker exec -i postgres psql -U program ratings < test/ratings.dump
 docker exec -i postgres psql -U program reservations < test/reservations.dump
+
+docker exec -i postgres psql -U program libraries -c "\dt"
+docker exec -i postgres psql -U program ratings -c "\dt"
+docker exec -i postgres psql -U program reservations -c "\dt"
